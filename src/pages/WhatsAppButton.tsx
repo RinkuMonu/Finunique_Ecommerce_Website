b5e381b128251f4a9fc15d74d075a7c0ad8af1f9
RinkuMonu <rinkuyadav9460@gmail.com>
2025-09-06 16:15:46 +0530

    changes

--- src/pages/WhatsAppButton.tsx
+++ src/pages/WhatsAppButton.tsx
@@ -2,17 +2,17 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const WhatsAppButton = () => {
   return (
-    <a
-      href="https://wa.me/+918003541414"  
-      target="_blank"
-      rel="noopener noreferrer"
-      className="fixed bottom-5 right-5 bg-green-500 text-white rounded-full p-4 shadow-lg transition-all hover:scale-105 hover:bg-green-600 z-50"
-      style={{
-        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)", // Optional shadow
-      }}
-    >
-      <FaWhatsapp size={30} />
-    </a>
+    // <a
+    //   href="https://wa.me/+918003541414"  
+    //   target="_blank"
+    //   rel="noopener noreferrer"
+    //   className="fixed bottom-5 right-5 bg-green-500 text-white rounded-full p-4 shadow-lg transition-all hover:scale-105 hover:bg-green-600 z-50"
+    //   style={{
+    //     boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)", // Optional shadow
+    //   }}
+    // >
+    //   <FaWhatsapp size={30} />
+    // </a>
   );
 };
 
